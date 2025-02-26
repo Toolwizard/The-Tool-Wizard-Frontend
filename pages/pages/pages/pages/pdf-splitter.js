6b9f6a6ca47bbba1842183a1562d808a885c8192
Toolwizard <kingdavid1889@hotmail.co.uk>
2025-02-26 13:45:42 +0000

    Create pdf-splitter.js

---
+++ pages/pages/pages/pages/pdf-splitter.js
@@ -0,0 +1,8 @@
+export default function PdfSplitter() {
+    return (
+        <div>
+            <h1>PDF Splitter</h1>
+            <p>Upload a PDF file and split it into pages.</p>
+        </div>
+    );
+}
