1348c21adcff834df7f15be1ee50fc5f219dd908
Toolwizard <kingdavid1889@hotmail.co.uk>
2025-02-26 15:21:23 +0000

    Create number-generator.js

---
+++ pages/number-generator.js
@@ -0,0 +1,8 @@
+export default function NumberGenerator() {
+    return (
+        <div>
+            <h1>Random Number Generator</h1>
+            <p>Generate a random number between a range.</p>
+        </div>
+    );
+}
