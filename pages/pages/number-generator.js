b466768365ff506443ef3fb653cd87c4e85bd51b
Toolwizard <kingdavid1889@hotmail.co.uk>
2025-02-26 13:51:28 +0000

    Delete pages/pages directory

--- pages/pages/number-generator.js
+++
@@ -1,8 +0,0 @@
-export default function NumberGenerator() {
-    return (
-        <div>
-            <h1>Random Number Generator</h1>
-            <p>Generate a random number between a range.</p>
-        </div>
-    );
-}
