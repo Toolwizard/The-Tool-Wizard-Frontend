f690c2a1c2c76b1b5ff3657647476f1251d3756a
Toolwizard <kingdavid1889@hotmail.co.uk>
2025-02-26 13:51:09 +0000

    Delete pages/pages/pages/pages directory

--- pages/pages/pages/pages/pdf-splitter.js
+++
@@ -1,8 +0,0 @@
-export default function PdfSplitter() {
-    return (
-        <div>
-            <h1>PDF Splitter</h1>
-            <p>Upload a PDF file and split it into pages.</p>
-        </div>
-    );
-}
