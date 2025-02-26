018b744a0cae421598a9e8b0da7962af8ec2832c
Toolwizard <kingdavid1889@hotmail.co.uk>
2025-02-26 15:23:54 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
     return (
         <div>
             <h1>The Tool Wizard</h1>
             <p>Useful tools, all in one place.</p>
+
+            <ul>
+                <li><Link href="/number-generator">Random Number Generator</Link></li>
+                <li><Link href="/timer">Countdown Timer</Link></li>
+                <li><Link href="/pdf-splitter">PDF Splitter</Link></li>
+            </ul>
         </div>
     );
 }
