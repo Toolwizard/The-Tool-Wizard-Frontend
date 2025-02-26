2470b267ab7e8f3fc4d5300b0ecb168b12781e06
Toolwizard <kingdavid1889@hotmail.co.uk>
2025-02-26 13:51:17 +0000

    Delete pages/pages/pages directory

--- pages/pages/pages/timer.js
+++
@@ -1,8 +0,0 @@
-export default function Timer() {
-    return (
-        <div>
-            <h1>Countdown Timer</h1>
-            <p>Start a timer and track countdown.</p>
-        </div>
-    );
-}
