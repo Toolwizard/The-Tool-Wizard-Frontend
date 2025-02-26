e8be0364f3555c40b6468eb951a3d839cf2784ba
Toolwizard <kingdavid1889@hotmail.co.uk>
2025-02-26 15:21:48 +0000

    Create timer.js

---
+++ pages/timer.js
@@ -0,0 +1,8 @@
+export default function Timer() {
+    return (
+        <div>
+            <h1>Countdown Timer</h1>
+            <p>Start a timer and track countdown.</p>
+        </div>
+    );
+}
